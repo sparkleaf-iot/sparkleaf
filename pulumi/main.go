d91034b3a3628b448dab29934d684c6a4229c7ec
Emil <emil.sallem@code.berlin>
2023-04-04 15:02:14 +0200

    added basic nomad provisionning in pulumi

--- pulumi/main.go
+++ pulumi/main.go
@@ -1,22 +1,50 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi-gcp/sdk/v5/go/gcp/compute"
+	nomad "github.com/pulumi/pulumi-nomad/sdk/go/nomad"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create a GCP resource (Storage Bucket)
-		bucket, err := storage.NewBucket(ctx, "my-bucket", &storage.BucketArgs{
-			Location: pulumi.String("US"),
+
+		// Create a new GCP network for the Nomad cluster.
+		network, err := compute.NewNetwork(ctx, "nomad-network", &compute.NetworkArgs{
+			AutoCreateSubnetworks: pulumi.Bool(true),
 		})
 		if err != nil {
 			return err
 		}
 
-		// Export the DNS name of the bucket
-		ctx.Export("bucketName", bucket.Url)
+		// Create a new GCP subnet for the Nomad cluster.
+		subnet, err := compute.NewSubnetwork(ctx, "nomad-subnet", &compute.SubnetworkArgs{
+			Network:       network.ID(),
+			IpCidrRange:   pulumi.String("10.0.0.0/24"),
+			Region:        pulumi.String("us-central1"),
+			PrivateIpGoogleAccess: pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+		// Create a new Nomad cluster.
+		cluster, err := nomad.NewCluster(ctx, "nomad-cluster", &nomad.ClusterArgs{
+			Datacenters: pulumi.StringArray{"dc1"},
+			Region:      pulumi.String("us-central1"),
+			SubnetIds:   pulumi.StringArray{subnet.ID()},
+			Node: &nomad.ClusterNodeArgs{
+				Count:          pulumi.Int(3),
+				MachineType:    pulumi.String("n1-standard-2"),
+				NetworkProfile: pulumi.String("custom"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Export the Nomad cluster address.
+		ctx.Export("nomad-address", cluster.HttpAddress)
+
 		return nil
 	})
 }
